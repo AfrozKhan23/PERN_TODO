40316baba863d327488f6977dc18353af5e796bc
Afroz233 <zackhan23@gmail.com>
2024-10-30 20:31:34 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,11 @@ import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://frontendtodopern.vercel.app",
+  })
+);
 
 app.use(express.json());
 
