69214260c568ddf2e9c4b67b99b0aa351d263614
Afroz Khan <121657466+AfrozKhan23@users.noreply.github.com>
2024-10-30 20:45:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,7 @@ import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: "https://frontendtodopern.vercel.app",
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
